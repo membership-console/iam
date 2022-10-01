bc50891eb3a15a89b8dd7fb9c6d7bd912ce88391
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2022-10-02 01:43:17 +0900

    [feature] ユニーク制約を追加

--- app/src/main/resources/db/migration/V1_0_3__create_r__user__user_group.sql
+++ app/src/main/resources/db/migration/V1_0_3__create_r__user__user_group.sql
@@ -3,12 +3,12 @@ CREATE TABLE IF NOT EXISTS `r__user__user_group`
     `user_id`       INT UNSIGNED NOT NULL,
     `user_group_id` INT UNSIGNED NOT NULL,
     PRIMARY KEY (`user_id`, `user_group_id`),
-    CONSTRAINT `fk_shop_user_group_member_shop_user_id`
+    CONSTRAINT `fk_r__user__user_group_user_id`
         FOREIGN KEY (`user_id`)
             REFERENCES `user` (`id`)
             ON DELETE CASCADE
             ON UPDATE NO ACTION,
-    CONSTRAINT `fk_shop_user_group_member_shop_user_group_id`
+    CONSTRAINT `fk_r__user__user_group_user_group_id`
         FOREIGN KEY (`user_group_id`)
             REFERENCES `user_group` (`id`)
             ON DELETE CASCADE
--- app/src/main/resources/db/migration/V1_0_4__create_user_group_role.sql
+++ app/src/main/resources/db/migration/V1_0_4__create_user_group_role.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `user_group_role`
     `user_group_id` INT UNSIGNED NOT NULL,
     `role_id`       INT UNSIGNED NOT NULL,
     PRIMARY KEY (`user_group_id`, `role_id`),
-    CONSTRAINT `fk_shop_user_group_role_shop_user_group_id`
+    CONSTRAINT `fk_user_group_role_user_group_id`
         FOREIGN KEY (`user_group_id`)
             REFERENCES `user_group` (`id`)
             ON DELETE CASCADE
