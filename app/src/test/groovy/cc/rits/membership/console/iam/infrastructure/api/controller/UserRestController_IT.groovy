251001eaf8fee3474a3db363bc5e67eb08770c32
averak <abe12@mccc.jp>
2022-10-06 03:47:02 +0900

    [test] ユーザ削除APIの統合テスト

--- app/src/test/groovy/cc/rits/membership/console/iam/infrastructure/api/controller/UserRestController_IT.groovy
+++ app/src/test/groovy/cc/rits/membership/console/iam/infrastructure/api/controller/UserRestController_IT.groovy
@@ -3,6 +3,7 @@ package cc.rits.membership.console.iam.infrastructure.api.controller
 import cc.rits.membership.console.iam.enums.Role
 import cc.rits.membership.console.iam.exception.ErrorCode
 import cc.rits.membership.console.iam.exception.ForbiddenException
+import cc.rits.membership.console.iam.exception.NotFoundException
 import cc.rits.membership.console.iam.exception.UnauthorizedException
 import cc.rits.membership.console.iam.helper.TableHelper
 import cc.rits.membership.console.iam.infrastructure.api.response.UserResponse
@@ -18,6 +19,7 @@ class UserRestController_IT extends AbstractRestController_IT {
     static final String BASE_PATH = "/api/users"
     static final String GET_USERS_PATH = BASE_PATH
     static final String GET_LOGIN_USER_PATH = BASE_PATH + "/me"
+    static final String DELETE_USER_PATH = BASE_PATH + "/%d"
 
     def "ユーザリスト取得API: 正常系 IAMの閲覧者がユーザリストを取得"() {
         given:
@@ -106,6 +108,88 @@ class UserRestController_IT extends AbstractRestController_IT {
     def "ログインユーザ取得API: 異常系 ログインしていない場合は401エラー"() {
         expect:
         final request = this.getRequest(GET_LOGIN_USER_PATH)
+        this.execute(request, new UnauthorizedException(ErrorCode.USER_NOT_LOGGED_IN))
+    }
+
+    def "ユーザ削除API: 正常系 IAMの管理者がユーザを削除"() {
+        given:
+        final user = this.login()
+
+        // @formatter:off
+        TableHelper.insert sql, "user_group", {
+            id | name
+            1  | ""
+        }
+        TableHelper.insert sql, "user_group_role", {
+            user_group_id | role_id
+            1             | Role.IAM_ADMIN.id
+        }
+        TableHelper.insert sql, "r__user__user_group", {
+            user_id | user_group_id
+            user.id | 1
+        }
+        // @formatter:on
+
+        when:
+        final request = this.deleteRequest(String.format(DELETE_USER_PATH, user.id))
+        this.execute(request, HttpStatus.OK)
+
+        then:
+        final users = sql.rows("SELECT * FROM user")
+        users == []
+    }
+
+    def "ユーザ削除API: 異常系 IAMの管理者以外は403エラー"() {
+        given:
+        final user = this.login()
+
+        // @formatter:off
+        TableHelper.insert sql, "user_group", {
+            id | name
+            1  | ""
+        }
+        TableHelper.insert sql, "user_group_role", {
+            user_group_id | role_id
+            1             | Role.IAM_VIEWER.id
+        }
+        TableHelper.insert sql, "r__user__user_group", {
+            user_id | user_group_id
+            user.id | 1
+        }
+        // @formatter:on
+
+        expect:
+        final request = this.deleteRequest(String.format(DELETE_USER_PATH, user.id))
+        this.execute(request, new ForbiddenException(ErrorCode.USER_HAS_NO_PERMISSION))
+    }
+
+    def "ユーザ削除API: 異常系 ユーザが存在しない場合は404エラー"() {
+        given:
+        final user = this.login()
+
+        // @formatter:off
+        TableHelper.insert sql, "user_group", {
+            id | name
+            1  | ""
+        }
+        TableHelper.insert sql, "user_group_role", {
+            user_group_id | role_id
+            1             | Role.IAM_ADMIN.id
+        }
+        TableHelper.insert sql, "r__user__user_group", {
+            user_id | user_group_id
+            user.id | 1
+        }
+        // @formatter:on
+
+        expect:
+        final request = this.deleteRequest(String.format(DELETE_USER_PATH, 0))
+        this.execute(request, new NotFoundException(ErrorCode.NOT_FOUND_USER))
+    }
+
+    def "ユーザ削除API: 異常系 ログインしていない場合は401エラー"() {
+        expect:
+        final request = this.deleteRequest(String.format(DELETE_USER_PATH, 1))
         this.execute(request, new UnauthorizedException(ErrorCode.USER_NOT_LOGGED_IN))
     }
 
