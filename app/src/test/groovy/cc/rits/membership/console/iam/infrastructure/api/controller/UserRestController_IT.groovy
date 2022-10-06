a503a2c8fc18a4e9b710f416f8c6fc41e697320e
averak <abe12@mccc.jp>
2022-10-06 10:51:46 +0900

    [feat] ユーザ取得APIの統合テスト

--- app/src/test/groovy/cc/rits/membership/console/iam/infrastructure/api/controller/UserRestController_IT.groovy
+++ app/src/test/groovy/cc/rits/membership/console/iam/infrastructure/api/controller/UserRestController_IT.groovy
@@ -19,6 +19,7 @@ class UserRestController_IT extends AbstractRestController_IT {
     static final String BASE_PATH = "/api/users"
     static final String GET_USERS_PATH = BASE_PATH
     static final String GET_LOGIN_USER_PATH = BASE_PATH + "/me"
+    static final String GET_USER_PATH = BASE_PATH + "/%d"
     static final String DELETE_USER_PATH = BASE_PATH + "/%d"
 
     def "ユーザリスト取得API: 正常系 IAMの閲覧者がユーザリストを取得"() {
@@ -108,6 +109,77 @@ class UserRestController_IT extends AbstractRestController_IT {
     def "ログインユーザ取得API: 異常系 ログインしていない場合は401エラー"() {
         expect:
         final request = this.getRequest(GET_LOGIN_USER_PATH)
+        this.execute(request, new UnauthorizedException(ErrorCode.USER_NOT_LOGGED_IN))
+    }
+
+    def "ユーザ取得API: 正常系 IAMの閲覧者がユーザリストを取得"() {
+        given:
+        final user = this.login()
+
+        // @formatter:off
+        TableHelper.insert sql, "user", {
+            id | first_name | last_name | email               | password | entrance_year
+            2  | ""         | ""        | "user2@example.com" | ""       | 2000
+        }
+        TableHelper.insert sql, "user_group", {
+            id | name
+            1  | "グループA"
+        }
+        TableHelper.insert sql, "user_group_role", {
+            user_group_id | role_id
+            1             | Role.IAM_VIEWER.id
+        }
+        TableHelper.insert sql, "r__user__user_group", {
+            user_id | user_group_id
+            user.id | 1
+        }
+        // @formatter:on
+
+        when:
+        final request = this.getRequest(String.format(GET_USER_PATH, 2))
+        final response = this.execute(request, HttpStatus.OK, UserResponse)
+
+        then:
+        response.id == 2
+        response.email == "user2@example.com"
+    }
+
+    def "ユーザ取得API: 異常系 ユーザが存在しない場合は404エラー"() {
+        given:
+        final user = this.login()
+
+        // @formatter:off
+        TableHelper.insert sql, "user_group", {
+            id | name
+            1  | "グループA"
+        }
+        TableHelper.insert sql, "user_group_role", {
+            user_group_id | role_id
+            1             | Role.IAM_VIEWER.id
+        }
+        TableHelper.insert sql, "r__user__user_group", {
+            user_id | user_group_id
+            user.id | 1
+        }
+        // @formatter:on
+
+        expect:
+        final request = this.getRequest(String.format(GET_USER_PATH, 2))
+        this.execute(request, new NotFoundException(ErrorCode.NOT_FOUND_USER))
+    }
+
+    def "ユーザ取得API: 異常系 IAMの閲覧者以外は403エラー"() {
+        given:
+        this.login()
+
+        expect:
+        final request = this.getRequest(String.format(GET_USER_PATH, 1))
+        this.execute(request, new ForbiddenException(ErrorCode.USER_HAS_NO_PERMISSION))
+    }
+
+    def "ユーザ取得API: 異常系 ログインしていない場合は401エラー"() {
+        expect:
+        final request = this.getRequest(String.format(GET_USER_PATH, 1))
         this.execute(request, new UnauthorizedException(ErrorCode.USER_NOT_LOGGED_IN))
     }
 
