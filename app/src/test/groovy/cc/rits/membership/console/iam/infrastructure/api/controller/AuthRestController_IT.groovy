30c0e31246df0c51cfdfcc1de087b329dd9f9bcb
averak <abe12@mccc.jp>
2022-10-12 13:56:25 +0900

    [test] パスワードリセット要求APIの統合テスト

--- app/src/test/groovy/cc/rits/membership/console/iam/infrastructure/api/controller/AuthRestController_IT.groovy
+++ app/src/test/groovy/cc/rits/membership/console/iam/infrastructure/api/controller/AuthRestController_IT.groovy
@@ -1,8 +1,10 @@
 package cc.rits.membership.console.iam.infrastructure.api.controller
 
+import cc.rits.membership.console.iam.exception.BadRequestException
 import cc.rits.membership.console.iam.exception.ErrorCode
 import cc.rits.membership.console.iam.exception.UnauthorizedException
 import cc.rits.membership.console.iam.infrastructure.api.request.LoginRequest
+import cc.rits.membership.console.iam.infrastructure.api.request.RequestPasswordResetRequest
 import org.springframework.http.HttpStatus
 
 /**
@@ -14,6 +16,7 @@ class AuthRestController_IT extends AbstractRestController_IT {
     static final String BASE_PATH = "/api"
     static final String LOGIN_PATH = BASE_PATH + "/login"
     static final String LOGOUT_PATH = BASE_PATH + "/logout"
+    static final String REQUEST_PASSWORD_RESET_PATH = BASE_PATH + "/request_password_reset"
 
     def "ログインAPI: 正常系 ログインに成功するとセッションにユーザ情報が記録される"() {
         given:
@@ -78,6 +81,36 @@ class AuthRestController_IT extends AbstractRestController_IT {
         expect:
         final request = this.postRequest(LOGOUT_PATH)
         this.execute(request, new UnauthorizedException(ErrorCode.USER_NOT_LOGGED_IN))
+    }
+
+    def "パスワードリセット要求API: 正常系 パスワードリセットを要求"() {
+        given:
+        final user = this.login()
+
+        final requestBody = RequestPasswordResetRequest.builder()
+            .email(user.email)
+            .build()
+
+        when:
+        final request = this.postRequest(REQUEST_PASSWORD_RESET_PATH, requestBody)
+        this.execute(request, HttpStatus.OK)
+
+        then:
+        final createdPasswordResetToken = sql.firstRow("SELECT * FROM password_reset_token")
+        createdPasswordResetToken.user_id == user.id
+    }
+
+    def "パスワードリセット要求API: 正常系 メールアドレスが存在しない場合は400エラー"() {
+        given:
+        final user = this.login()
+
+        final requestBody = RequestPasswordResetRequest.builder()
+            .email(user.email + "...")
+            .build()
+
+        expect:
+        final request = this.postRequest(REQUEST_PASSWORD_RESET_PATH, requestBody)
+        this.execute(request, new BadRequestException(ErrorCode.REQUESTED_EMAIL_IS_NOT_EXISTS))
     }
 
 }
