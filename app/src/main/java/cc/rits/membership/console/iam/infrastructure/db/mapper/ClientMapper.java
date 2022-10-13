48af31c754d6764a1899d604df0ce697dcac29ae
averak <abe12@mccc.jp>
2022-10-13 10:37:26 +0900

    [feat] clientテーブルを作成

---
+++ app/src/main/java/cc/rits/membership/console/iam/infrastructure/db/mapper/ClientMapper.java
@@ -0,0 +1,6 @@
+package cc.rits.membership.console.iam.infrastructure.db.mapper;
+
+import cc.rits.membership.console.iam.infrastructure.db.mapper.base.ClientBaseMapper;
+
+public interface ClientMapper extends ClientBaseMapper {
+}
