98bf5d8a33296b8ad13c5ebed279a95dc5a57455
averak <abe12@mccc.jp>
2022-10-27 17:26:44 +0900

    [feat] client credentials grant flowを実装

---
+++ app/src/main/java/cc/rits/membership/console/iam/config/AuthorizationServerConfig.java
@@ -0,0 +1,114 @@
+package cc.rits.membership.console.iam.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.time.Duration;
+import java.util.UUID;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
+import org.springframework.security.oauth2.server.authorization.config.TokenSettings;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 認可サーバの設定
+ */
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class AuthorizationServerConfig {
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authServerSecurityFilterChain(final HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        return http.formLogin(withDefaults()).build();
+    }
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated()).formLogin(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        // TODO: これは消す
+        final var registeredClient = RegisteredClient.withId(UUID.randomUUID().toString()) //
+            .clientId("client1") //
+            .clientSecret(this.passwordEncoder.encode("password")) //
+            .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS) //
+            .scope("user") //
+            .scope("me") //
+            .tokenSettings(this.tokenSettings()) //
+            .build();
+
+        final var registeredClientRepository = new JdbcRegisteredClientRepository(this.jdbcTemplate);
+        registeredClientRepository.save(registeredClient);
+
+        return registeredClientRepository;
+    }
+
+    @Bean
+    public ProviderSettings providerSettings() {
+        return ProviderSettings.builder().build();
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        final var rsaKey = this.generateRsa();
+        final var jwkSet = new JWKSet(rsaKey);
+        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+    }
+
+    private RSAKey generateRsa() {
+        final var keyPair = this.generateRsaKey();
+        final var publicKey = (RSAPublicKey) keyPair.getPublic();
+        final var privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        return new RSAKey.Builder(publicKey).privateKey(privateKey).keyID(UUID.randomUUID().toString()).build();
+    }
+
+    private KeyPair generateRsaKey() {
+        try {
+            final var keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            return keyPairGenerator.generateKeyPair();
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
+
+    private TokenSettings tokenSettings() {
+        return TokenSettings.builder() //
+            .accessTokenTimeToLive(Duration.ofSeconds(900)) //
+            .build();
+    }
+
+}
